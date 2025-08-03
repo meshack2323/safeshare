5ba1ce1c0acf80fe76100dfb1b99b2125c908397
meshack2323 <168490725+meshack2323@users.noreply.github.com>
2025-08-04 00:58:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,8 @@ def download(msg_id):
     return abort(403)  # Downloads not allowed via direct link
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+import os
+
+port = int(os.environ.get("PORT",5000))
+app.run(host="0.0.0.0", port=port)
