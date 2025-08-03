6d2444ffa38304cd856175efb2dbd7628072f724
meshack2323 <168490725+meshack2323@users.noreply.github.com>
2025-08-04 01:03:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def download(msg_id):
 
 if __name__ == '__main__':
 
-import os
+    import os
 
-port = int(os.environ.get("PORT",5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
